0d4a9b9359110bcdfe3c66ee640130efce80b478
amarender4321 <bavajiamarender@gmail.com>
2022-07-25 15:58:20 +0530

    5th commit

--- src/test/java/gitpratice/GitCommand_2.java
+++ src/test/java/gitpratice/GitCommand_2.java
@@ -7,7 +7,8 @@ System.out.println("new_git");
 System.out.println("git_1");
 System.out.println("git_2");
 		System.out.println("amar");
-System.out.println("change branch");		
+System.out.println("change branch");
+         System.out.println("again change");
 	}
 
 }
