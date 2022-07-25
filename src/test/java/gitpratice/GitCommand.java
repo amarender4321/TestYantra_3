c2169c433ab4965b9937585c06959f2941df93e3
amarender4321 <bavajiamarender@gmail.com>
2022-07-25 12:56:06 +0530

    my commit

---
+++ src/test/java/gitpratice/GitCommand.java
@@ -0,0 +1,8 @@
+package gitpratice;
+
+public class GitCommand {
+	public static void main(String[] args) {
+		System.out.println("command add");
+	}
+
+}
---
+++ src/test/java/gitpratice/GitCommand_2.java
@@ -0,0 +1,10 @@
+package gitpratice;
+
+public class GitCommand_2 {
+
+	public static void main(String[] args) {
+System.out.println("new_git");
+System.out.println("git");
+	}
+
+}
