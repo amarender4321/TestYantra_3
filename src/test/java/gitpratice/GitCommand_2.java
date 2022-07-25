740866cff8d01dd733222142a641adcf51f08a77
amarender4321 <bavajiamarender@gmail.com>
2022-07-25 13:08:29 +0530

    again commit

--- src/test/java/gitpratice/GitCommand_2.java
+++ src/test/java/gitpratice/GitCommand_2.java
@@ -4,7 +4,8 @@ public class GitCommand_2 {
 
 	public static void main(String[] args) {
 System.out.println("new_git");
-System.out.println("git");
+System.out.println("git_1");
+System.out.println("git_2");
 	}
 
 }
